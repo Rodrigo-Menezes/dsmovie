9876d5237904108d0fab88e97a920efa9b917e50
RodrigoMenezes <rodrigo_menezes91@hotmail.com>
2022-03-08 10:49:07 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+import{ReactComponent as StarFull} from 'assets/img/StarFull.svg';
+import{ReactComponent as StarHalf} from 'assets/img/StarHalf.svg';
+import{ReactComponent as StarEmpty} from 'assets/img/StarEmpty.svg';
+function MovieSatars() {
+    return (<div className="dsmovie-stars-container">
+        <StarFull />
+        <StarFull />
+        <StarFull />
+        <StarHalf />
+        <StarEmpty />
+    </div>
+    );
+}
+export default MovieSatars;
